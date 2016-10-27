4bba9cf6395b49a6f664c1f0dec1a7d0265966e0
Justin D'Errico <justin.derrico1991@gmail.com>
2016-10-27 19:02:54 -0400

    sentence_destructure

--- balls.py
+++ balls.py
@@ -4,6 +4,15 @@ import paths
 import nltk
 from nltk import *
 
+
+def destructure_sentence(sentence):
+    currentSentence = []
+    for node in sentence:
+        currentSentence.append((node[0], str(node[1]).split("'")[1]))
+
+    return currentSentence
+
+
 # Grammar rules
 with open (paths.DICTIONARY_FILE, "r") as myfile:
     grammaireText = myfile.read()
@@ -15,12 +24,18 @@ with open (paths.STORY_FILE, "r") as myfile:
 grammar = grammar.FeatureGrammar.fromstring(grammaireText)
 parser = nltk.ChartParser(grammar)
 sentences = textSource[:-1].split('.')
+sentenceTrace = []
+
 for sentence in sentences:
     print(sentence)
     tokens = sentence.split()
     parser = parse.FeatureEarleyChartParser(grammar)
     trees = parser.parse(tokens)
-    for tree in trees:
+    for index, tree in enumerate(trees):
         print(tree)
+        if index == 0:
+            sentenceTrace.append(destructure_sentence(tree.pos()))
         #nltk.draw.tree.draw_trees(tree)
         #print(tree.label()['SEM'])
+
+print(sentenceTrace)
