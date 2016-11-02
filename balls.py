f57b382c751ae298b0e8022cad1059f9fe321601
Justin D'Errico <justin.derrico1991@gmail.com>
2016-11-02 00:13:42 -0400

    finished pronounws

--- balls.py
+++ balls.py
@@ -14,11 +14,12 @@ def getParams(string, shouldExistList):
     for existString in shouldExistList:
         string = string.replace(existString.text, "")
 
-    return string.split(',')
+    return string.split(",")
+
 
 def check(smegmantique, shouldExistList):
     sentence = "("
-    for removeString in ['(', ')']:
+    for removeString in ["(", ")"]:
         smegmantique = smegmantique.replace(removeString, "")
 
     params = getParams(smegmantique, shouldExistList)
@@ -30,25 +31,35 @@ def check(smegmantique, shouldExistList):
 
     return sentence[:-1] + ")"
 
+
 def destructure_sentence(sentence):
     currentSentence = []
     for node in sentence:
-        currentSentence.append((node[0], str(node[1]).split("'")[1]))
+        nodeDef = "".join(str(node[1]).split())[1:-1].replace("[", "") #remove whitespaces and split for ','
+        nodeType = str(node[1]).split("'")[1]
+        nodeDef = nodeDef.split("]")
+        hasCategoryFlag = [s for s in nodeDef if "CAT=" in s] #only the words with a CAT flag
+        category = ""
+        if (len(hasCategoryFlag) > 0):
+            category = hasCategoryFlag[0][:-1].split("'")[1]
+        currentSentence.append((node[0], nodeType, category)) #(word, wordtype, category)
 
     return currentSentence
 
+
 def check_rules(fileHandler, smegmantique):
-    jess_rule = check(smegmantique, [Smegment('opt', 'personnage'), Smegment('man', 'possede')])
+    jess_rule = check(smegmantique, [Smegment("opt", "personnage"), Smegment("man", "possede")])
     if jess_rule != -1:
       fileHandler.write(jess_rule)
 
-    jess_rule = check(smegmantique, [Smegment('opt', 'cours'), Smegment('man', 'apprend')])
+    jess_rule = check(smegmantique, [Smegment("opt", "cours"), Smegment("man", "apprend")])
     if jess_rule != -1:
       fileHandler.write(jess_rule)
+
 
 def sentence_parser(sentences):
     curSentences = list(sentences)
-    lastSubject = ''
+    lastSubject = ""
 
     for sentenceIndex, sentence in enumerate(curSentences):
         tokens = sentence.split()
@@ -59,11 +70,13 @@ def sentence_parser(sentences):
                 for wordTuple in destructured:
                     word = wordTuple[0]
                     wordType = wordTuple[1]
-                    if (wordType == 'NProp'):
+                    category = wordTuple[2]
+                    if (wordType == "NProp" and category == 'v'):
                         lastSubject = word
-                    elif (wordType == 'Pro'):
+                    elif (wordType == "Pro"):
                         curSentences[sentenceIndex] = sentence.replace(word, lastSubject)
     return curSentences
+
 
 # Grammar rules
 with open (paths.DICTIONARY_FILE_PATH, "r") as myfile:
@@ -77,7 +90,7 @@ fileHandler = FileHandler(paths.FACTS_FILE_PATH)
 
 grammar = grammar.FeatureGrammar.fromstring(grammaireText)
 parser = parse.FeatureEarleyChartParser(grammar)
-sentences = textSource.split('.')[:-1] #removes last item from list, an empty string because of last split
+sentences = textSource.split(".")[:-1] #removes last item from list, an empty string because of last split
 sentences = sentence_parser(sentences)
 
 for sentence in sentences:
@@ -85,6 +98,6 @@ for sentence in sentences:
     trees = parser.parse(tokens)
     nltk.draw.tree.draw_trees(tree)
     for index, tree in enumerate(trees):
-        check_rules(fileHandler, str(tree.label()['SEM']))
+        check_rules(fileHandler, str(tree.label()["SEM"]))
 
 fileHandler.dispose()
